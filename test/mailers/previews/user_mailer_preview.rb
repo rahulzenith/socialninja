212f034dabd3bc9f76aeed66651e2c6f96bc32f8
Rahul uprety <rahul.uprety0@gmail.com>
2018-08-11 01:02:25 +0530

    mailers_preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,7 +3,7 @@ class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/new_comment
   def new_comment
-    UserMailer.new_comment
+    UserMailer.new_comment(Comment.last.id)
   end
 
 end
